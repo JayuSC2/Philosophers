b165f3084ae93cad15d760feffed5f061e09b517
Jayu <jayuze@gmail.com>
2024-05-09 18:57:50 +0200

    idk

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 18:18:13 by juitz             #+#    #+#             */
-/*   Updated: 2024/05/08 14:36:17 by juitz            ###   ########.fr       */
+/*   Updated: 2024/05/09 18:53:35 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 		return (ft_error("Error: Incorrect amount of arguments"), 1);
 	philo.philo_count = ft_atoi(argv[1]);
 	philo.forks = philo.philo_count;
-	create_IDs(&philo);
+	//create_IDs(&philo);
 	philo.time_to_die = ft_atoi(argv[2]);
 	philo.time_to_eat = ft_atoi(argv[3]);
 	philo.time_to_sleep = ft_atoi(argv[4]);
