e799967fd74e099d2b0010063d2a194b3575b5fe
Jayu <jayuze@gmail.com>
2024-08-23 11:41:29 +0200

    fixed return statements/values

--- src/create_philo.c
+++ src/create_philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   create_philo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
+/*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:09:30 by juitz             #+#    #+#             */
-/*   Updated: 2024/08/22 13:37:31 by julian           ###   ########.fr       */
+/*   Updated: 2024/08/23 11:33:20 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	create_philo(t_metadata *m_data)
         m_data->philo[i].id = i + 1;
         m_data->philo[i].time = m_data->time;
         if (pthread_create(&threads[i], NULL, &routine, &m_data->philo[i]) != 0)
-            printf("Error creating thread\n");
+            return(1);
         i++;
     }
     m_data->time->start_time = get_current_time();
