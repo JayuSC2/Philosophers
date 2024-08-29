5e6a971e22489920918ac6426801a7d0db607049
Jayu <jayuze@gmail.com>
2024-08-29 19:17:31 +0200

    still need to fix some issues from benjamin's git

--- src/monitoring.c
+++ src/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:36:27 by juitz             #+#    #+#             */
-/*   Updated: 2024/08/29 18:00:28 by juitz            ###   ########.fr       */
+/*   Updated: 2024/08/29 19:15:30 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int monitoring(t_metadata *m_data)
         {
             m_data->all_full = true;
         }
-        //usleep(1000);
+        usleep(1000);
     }
     return (0);
 }
